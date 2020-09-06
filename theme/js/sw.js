63e175124b23b054cfe5e847dbb2cec7868d7b90
carab88 <49131160+carab88@users.noreply.github.com>
2020-09-06 14:21:17 +0900

    Update sw.js

--- theme/js/sw.js
+++ theme/js/sw.js
@@ -1,22 +1,2 @@
 self.addEventListener('fetch', function(event) {
 });
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('store').then(function(cache) {
-     return cache.addAll([
-       '/index.html'
-            ]);
-   })
- );
-});
-
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-
