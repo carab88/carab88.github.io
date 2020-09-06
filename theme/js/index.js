42f1b0264d682e97bb315622e015b995c609064a
carab88 <49131160+carab88@users.noreply.github.com>
2020-09-06 13:40:03 +0900

    Update index.js

--- theme/js/index.js
+++ theme/js/index.js
@@ -1,7 +1,7 @@
 // サービスワーカーの登録
 window.addEventListener('load', function() {
 	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('sw.js')
+		navigator.serviceWorker.register('./sw.js')
 			.then(function(registration) {
 				console.log('serviceWorker registed.');
 			}).catch(function(error) {
