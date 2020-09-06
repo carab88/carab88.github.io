34689e92bc383980279e85045b0700e04d98062e
carab88 <49131160+carab88@users.noreply.github.com>
2020-09-06 13:31:48 +0900

    Create index.js

---
+++ theme/js/index.js
@@ -0,0 +1,19 @@
+// サービスワーカーの登録
+window.addEventListener('load', function() {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('service-worker.js')
+			.then(function(registration) {
+				console.log('serviceWorker registed.');
+			}).catch(function(error) {
+				console.warn('serviceWorker error.', error);
+			})
+		;
+	}
+});
+
+
+// UAの出力
+window.addEventListener('load', function() {
+	var target = document.getElementById('ua');
+	target.textContent = navigator.userAgent;
+});
